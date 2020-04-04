c1e4624fd2031c3f7f7206e60c191d94c5a933fc
Rupert Barrow <48629751+RupertBarrow@users.noreply.github.com>
2020-04-04 22:49:56 +0200

    Update orgInit.sh

--- orgInit.sh
+++ orgInit.sh
@@ -1,8 +1,3 @@
-sfdx plugins:link node_modules/shane-sfdx-plugins
-sfdx plugins:link node_modules/@salesforce/analytics
-sfdx plugins:link node_modules/@mshanemc/sfdx-migration-automatic
-sfdx force:auth:jwt:grant --clientid 3MVG9T46ZAw5GTfVmqwOUu5uB8FfzS7gF66q9owrLElV_6zoyEF7jS8lNI2tjr3Me1xxVIG.IxeSfP74vUPld -u rupert.barrow@lilo.org --jwtkeyfile /app/tmp/server.key -s -a
-
 sfdx shane:org:create -f config/project-scratch-def.json -s -d 1 --userprefix crash --userdomain course.org
 sfdx force:source:push
 sfdx force:user:permset:assign -n VolunteeringApp
